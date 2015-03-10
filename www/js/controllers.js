3edd49337b1de657416c69fa72a81fd624c9d4fe
nbbenedict <noah@thebenedicts.net>
2015-03-10 14:50:47 -0600

    in progress work on the transfer page

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,46 +1,75 @@
 angular.module('starter.controllers', [])
 
 .controller('AppCtrl', function($scope, $ionicModal, $timeout) {
-  // Form data for the login modal
-  $scope.loginData = {};
+    // Form data for the login modal
+    $scope.loginData = {};
 
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
+    // Create the login modal that we will use later
+    $ionicModal.fromTemplateUrl('templates/login.html', {
+        scope: $scope
+    }).then(function(modal) {
+        $scope.modal = modal;
+    });
 
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-    $scope.modal.hide();
-  };
+    // Triggered in the login modal to close it
+    $scope.closeLogin = function() {
+        $scope.modal.hide();
+    };
 
-  // Open the login modal
-  $scope.login = function() {
-    $scope.modal.show();
-  };
+    // Open the login modal
+    $scope.login = function() {
+        $scope.modal.show();
+    };
 
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
+    // Perform the login action when the user submits the login form
+    $scope.doLogin = function () {
+        console.log('Doing login', $scope.loginData);
 
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
-  };
+        // Simulate a login delay. Remove this and replace with your login
+        // code if using a login system
+        $timeout(function () {
+            $scope.closeLogin();
+        }, 1000);
+    };
+
+    // Create the AccountSelector Modal Dialog
+    $ionicModal.fromTemplateUrl('templates/accountselector.html', {
+        scope: $scope
+    }).then(function (modal) {
+        $scope.accountSelector = modal;
+    });
+
+    // Triggered in the accountSelector modal to close it
+    $scope.closeAccountSelector = function () {
+        $scope.accountSelector.hide();
+    };
+
+
+    $scope.tasks = [
+      { title: 'Collect coins test' },
+      { title: 'Eat mushrooms' },
+      { title: 'Get high enough to grab the flag' },
+      { title: 'Find the Princess' }
+    ];
+
+    $scope.accounts = [
+      { accountId: 1, owner: 'Bauer Leasing Inc', accountNumber: '**61500', accountDescription: 'CIH SPRAYER'},
+      { accountId: 2, owner: 'Bauer Leasing Inc', accountNumber: '**77301', accountDescription: '3320 SPRAYER' },
+      { accountId: 3, owner: 'D & K Larsen Farms Inc', accountNumber: '**36399', accountDescription: 'Svc Acct' },
+      { accountId: 4, owner: 'D & K Larsen Farms Inc', accountNumber: '**67200', accountDescription: 'Operating' },
+      { accountId: 5, owner: 'D & K Larsen Farms Inc', accountNumber: '**30103', accountDescription: '07 Cat Trctr' },
+      { accountId: 6, owner: 'D & K Larsen Farms Inc', accountNumber: '**80501', accountDescription: '8360 Tractor' },
+      { accountId: 7, owner: 'Dane M Larsen', accountNumber: '**11699', accountDescription: 'Svc Acct' },
+      { accountId: 8, owner: 'Dane M Larsen', accountNumber: '**28101', accountDescription: 'Bin & Tile' },
+      { accountId: 9, owner: 'Dane M Larsen', accountNumber: '**25501', accountDescription: 'S 5-139-50' }
+    ];
+
+    $scope.transfer = {
+        fromAccountId: 0,
+        toAccountId: 0,
+        amount: 0,
+        comment: null
+    };
 })
 
 
-
-.controller('transferAccounts', function($scope) {
-    $scope.transferAccounts = [
-        { name: '0000109xxx - RIB03xxxx Available Balance: 0.00', id: 1 },
-        { name: '0000205xxx - RIB03xxxx Available Balance: 0.00', id: 2 },
-        { name: '0000426xxx - RIB03xxxx Available Balance: 0.00', id: 3 },
-        { name: '0000525xxx - RIB03xxxx Available Balance: 0.00', id: 4 },
-        { name: '0000709xxx - RIB03xxxx Available Balance: 0.00', id: 5 }
-    ];
-});
